a6acb00b527b85eebc7b3aac405ec57848840230
dunof <36583033+dunof@users.noreply.github.com>
2018-03-03 19:50:30 -0300

    esx
    esx + plugins

---
+++ resources/[essential]/es_extended/config.lua
@@ -0,0 +1,12 @@
+Config                          = {}
+Config.MaxPlayers               = 32
+Config.Accounts                 = { 'bank', 'black_money' }
+Config.AccountLabels            = { bank = 'Banco', black_money = 'Dinheiro Sujo' } -- French
+-- Config.AccountLabels            = { bank = 'Bank', black_money = 'Dirty Money' } -- English
+Config.PaycheckInterval         = 7 * 60000
+Config.ShowDotAbovePlayer       = false
+Config.DisableWantedLevel       = true
+Config.RemoveInventoryItemDelay = 5 * 60000
+Config.EnableWeaponPickup       = true
+Config.Locale                   = 'br'
+Config.EnableDebug              = false
