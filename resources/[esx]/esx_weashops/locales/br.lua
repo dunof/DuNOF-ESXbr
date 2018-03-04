318c46e2eb96f18f7dbdb3aba46137fd0f660501
dunof <36583033+dunof@users.noreply.github.com>
2018-03-04 00:06:31 -0300

    banking and weashops

---
+++ resources/[esx]/esx_weashops/locales/br.lua
@@ -0,0 +1,13 @@
+Locales ['en'] = {
+
+  ['buy_license'] = 'comprar porte de arma?',
+  ['yes'] = 'sim',
+  ['no'] = 'não',
+  ['buy'] = 'você comprou',
+  ['not_enough_black'] = 'você não tem dinheiro sujo suficiente',
+  ['not_enough'] = 'você não tem dinheiro suficiente',
+  ['shop'] = 'comprar',
+  ['shop_menu'] = 'pressione ~INPUT_CONTEXT~ para acessar o menu.',
+  ['map_blip'] = 'Loja de Armas',
+
+}
