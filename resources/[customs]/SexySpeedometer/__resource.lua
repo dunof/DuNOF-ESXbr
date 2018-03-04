73c243ff1f2a0d55f511f11821ac08d6780b15fa
dunof <36583033+dunof@users.noreply.github.com>
2018-03-04 20:42:48 -0300

    some customs
    sexyspeedo + synn's loadscreen

---
+++ resources/[customs]/SexySpeedometer/__resource.lua
@@ -0,0 +1,21 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+client_scripts {
+	"client.lua",
+
+	-- add skins here
+	"skins/id7.lua",
+
+
+}
+
+exports {
+	"getAvailableSkins",
+	"changeSkin",
+	"addSkin",
+	"toggleSpeedo",
+	"getCurrentSkin",
+	"addSkin",
+	"toggleFuelGauge",
+	"DoesSkinExist",
+}
