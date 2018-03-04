fda4c1c8c6b1eca734c294a9346a9f3c254f7080
dunof <36583033+dunof@users.noreply.github.com>
2018-03-04 20:22:03 -0300

    esx_dmvscholl and voice

---
+++ resources/[esx]/esx_dmvschool/localization/br_esx_dmvschool.sql
@@ -0,0 +1,8 @@
+USE `essentialmode`;
+
+INSERT INTO `licenses` (`type`, `label`) VALUES
+	('dmv', 'DETRAN'),
+	('drive', 'CNH'),
+	('drive_bike', 'CNH Motocicleta'),
+	('drive_truck', 'CNH Comercial')
+;
