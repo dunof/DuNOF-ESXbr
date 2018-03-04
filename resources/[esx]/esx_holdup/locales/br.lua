cef5236c3a2d760d54afd1bf342a3fe641b97748
dunof <36583033+dunof@users.noreply.github.com>
2018-03-04 14:10:24 -0300

    holdup

---
+++ resources/[esx]/esx_holdup/locales/br.lua
@@ -0,0 +1,24 @@
+Locales['br'] = {
+
+    ['robbery_cancelled'] = 'o assalto será cancelado, você não ganhará nada!',
+    ['robbery_successful'] = 'o roubo foi um sucesso! você ganhou ~g~$',
+    ['shop_robbery'] = 'loja roubável',
+    ['press_to_rob'] = 'pressione ~INPUT_CONTEXT~ para roubar ~b~',
+    ['robbery_of'] = 'roubar loja: ~r~',
+    ['seconds_remaining'] = '~w~ segundos restantes',
+    ['robbery_cancelled_at'] = '~r~ o assalto será em: ~b~',
+    ['robbery_has_cancelled'] = '~r~ O assalto foi cancelado: ~b~',
+    ['already_robbed'] = 'esta loja foi roubada recentemente... por favor, espere: ',
+    ['seconds'] = 'segundos.',
+    ['rob_in_prog'] = '~r~ assalto em curso em: ~b~',
+    ['started_to_rob'] = 'você começou a roubar ~b~',
+    ['do_not_move'] = '~s~, não se afaste!',
+    ['alarm_triggered'] = 'o alarme foi disparado',
+    ['hold_pos'] = 'segure por 5 minutos e o dinheiro é seu!',
+    ['robbery_complete'] = '~r~ roubo completo.~s~ ~h~ corre!',
+    ['robbery_complete_at'] = '~r~ Roubo completo em: ~b~',
+    ['min_police'] = 'necessário pelo menos ~b~',
+    ['in_city'] = '~s~ policiais na cidade para executar assaltos.',
+    ['robbery_already'] = '~r~um assalto já está em andamento.',
+
+}
