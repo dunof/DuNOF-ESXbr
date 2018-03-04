94c2976909f3413c216e5b3f0abf93775ab98847
dunof <36583033+dunof@users.noreply.github.com>
2018-03-04 20:22:29 -0300

    bmw1200 del

--- resources/[vehicles]/bmwgs1200/__resource.lua
+++
@@ -1,9 +0,0 @@
-resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
-
-data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
-
-files {
-  'data/vehicles.meta',
-}
-
-client_script 'vehicle_names.lua'
