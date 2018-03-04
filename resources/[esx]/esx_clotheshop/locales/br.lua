6f52c584e9507cb0b97cf9fc8415e32541600ce4
dunof <36583033+dunof@users.noreply.github.com>
2018-03-03 23:35:38 -0300

    shops

---
+++ resources/[esx]/esx_clotheshop/locales/br.lua
@@ -0,0 +1,13 @@
+Locales['br'] = {
+
+	['valid_this_purchase'] = 'confirmar esta compra?',
+	['yes'] = 'sim',
+	['no'] = 'não',
+	['name_outfit'] = 'nome do traje?',
+	['not_enough_money'] = 'você não possui dinheiro suficiente',
+	['press_menu'] = 'pressione ~INPUT_CONTEXT~ para acessar o menu',
+	['clothes'] = 'roupas',
+	['you_paid'] = 'você pagou $',
+	['save_in_dressing'] = 'você deseja salvar este traje?',
+
+}
