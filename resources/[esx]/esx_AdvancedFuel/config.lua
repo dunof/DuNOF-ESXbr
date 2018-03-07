cdabf5c13b64c5879a6565fe311e8db2b6dde299
dunof <36583033+dunof@users.noreply.github.com>
2018-03-07 14:07:47 -0300

    advancedfuel
    remove frfuel and trying advancedfuel

---
+++ resources/[esx]/esx_AdvancedFuel/config.lua
@@ -0,0 +1,79 @@
+petrolCanPrice = 1
+
+-- lang = "en"
+lang = "br"
+
+settings = {}
+settings["en"] = {
+	openMenu = "Press ~g~E~w~ to open the menu.",
+	electricError = "~r~You have an electric vehicle.",
+	fuelError = "~r~You're not in the good place.",
+	buyFuel = "buy fuel",
+	liters = "liters",
+	percent = "percent",
+	confirm = "Confirm",
+	fuelStation = "Fuel station",
+	boatFuelStation = "Fuel station | Boat",
+	avionFuelStation = "Fuel station | Plane ",
+	heliFuelStation = "Fuel station | Helicopter",
+	getJerryCan = "Press ~g~E~w~ to buy a Petrol can ("..petrolCanPrice.."$)",
+	refeel = "Press ~g~E~w~ to refeel the car.",
+	YouHaveBought = "You have bought ",
+	fuel = " liters of fuel",
+	price = "price"
+}
+
+settings["fr"] = {
+	openMenu = "Appuyez sur ~g~E~w~ pour ouvrir le menu.",
+	electricError = "~r~Vous avez une voiture électrique.",
+	fuelError = "~r~Vous n'êtes pas au bon endroit.",
+	buyFuel = "acheter de l'essence",
+	liters = "litres",
+	percent = "pourcent",
+	confirm = "Valider",
+	fuelStation = "Station essence",
+	boatFuelStation = "Station d'essence | Bateau",
+	avionFuelStation = "Station d'essence | Avions",
+	heliFuelStation = "Station d'essence | Hélicoptères",
+	getJerryCan = "Appuyez sur ~g~E~w~ pour acheter un bidon d'essence ("..petrolCanPrice.."$)",
+	refeel = "Appuyez sur ~g~E~w~ pour remplir votre voiture d'essence.",
+	YouHaveBought = "Vous avez acheté ",
+	fuel = " litres d'essence",
+	price = "prix"
+}
+
+settings["br"] = {
+	openMenu = "pressione ~g~E~w~ para abrir o menu.",
+	electricError = "~r~Você tem um veículo elétrico.",
+	fuelError = "~r~Você não está no lugar correto.",
+	buyFuel = "comprar combustível",
+	liters = "litros",
+	percent = "porcento",
+	confirm = "Confirmar",
+	fuelStation = "Posto de Gasolina",
+	boatFuelStation = "Posto de Gasolina | Barcos",
+	avionFuelStation = "Posto de Gasolina | Aviões ",
+	heliFuelStation = "Posto de Gasolina | Helicópteros",
+	getJerryCan = "pressione ~g~E~w~ para comprar um galão de combustível por: ($"..petrolCanPrice..")",
+	refeel = "pressione ~g~E~w~ para reabastecer o carro.",
+	YouHaveBought = "Você comprou ",
+	fuel = " litros de gasolina",
+	price = "pre"
+}
+
+showBar = true
+showText = false
+
+
+hud_form = 1 -- 1 : Vertical | 2 = Horizontal
+hud_x = 0.175 
+hud_y = 0.885
+
+text_x = 0.2575
+text_y = 0.975
+
+
+electricityPrice = 1 -- NOT RANOMED !!
+
+randomPrice = false --Random the price of each stations
+price = 4 --If random price is on False, set the price here for 1 liter
