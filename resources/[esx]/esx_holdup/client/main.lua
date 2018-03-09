3e706f8a0928e87c5e5fdfd0e9a814e037da2086
dunof <36583033+dunof@users.noreply.github.com>
2018-03-09 15:55:49 -0300

    ArmedRobery Snippet
    esx_holdup

--- resources/[esx]/esx_holdup/client/main.lua
+++ resources/[esx]/esx_holdup/client/main.lua
@@ -126,7 +126,18 @@ Citizen.CreateThread(function()
                         end
                         incircle = true
                         if IsControlJustReleased(1, 51) then
-                            TriggerServerEvent('esx_holdup:rob', k)
+                            local weaponStealeableList = {453432689, 1593441988, 584646201, 2578377531, 324215364, 736523883, 4024951519, 3220176749, 2210333304, 2937143193, 2634544996, 2144741730, 487013001} -- https://www.se7ensins.com/forums/threads/weapon-and-explosion-hashes-list.1045035/ weapon's codes
+                            for i=1, #weaponStealeableList do
+                                if GetCurrentPedWeapon(GetPlayerPed(-1), weaponStealeableList[i], false) then
+                                   haveWeapon = true
+                                end
+                            end
+
+                            if(haveWeapon)then
+                                TriggerServerEvent('esx_holdup:rob', k)
+                            else
+                                ESX.ShowNotification("Você precisa estar armado para executar um assalto!") -- Here for changed your notification
+                            end
                         end
                     elseif(Vdist(pos.x, pos.y, pos.z, pos2.x, pos2.y, pos2.z) > 1.0)then
                         incircle = false
