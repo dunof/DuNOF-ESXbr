f4194493f0481def9b35ae6cd2c79bd8278cfd78
dunof <36583033+dunof@users.noreply.github.com>
2018-03-09 16:18:04 -0300

    esx_blanchisseur

---
+++ resources/[esx]/esx_blanchisseur/locales/br.lua
@@ -0,0 +1,13 @@
+Locales['br'] = {
+	['blanchisseurblip'] 	= 'Lavar Dinheiro',
+	['press_menu'] 			= 'pressione ~INPUT_CONTEXT~ para lavar seu ~y~Dinheiro Sujo',
+	['Nothere'] 			= 'volte mais tarde...',
+	['Whitening'] 			= 'processo em andamento...',
+	['Nocash'] 				= 'você não tem dinheiro suficiente para lavar, o mínimo é: R$',
+	['cash'] 				= 'você recebeu: ~r~R$ ',
+	['cash1'] 				= ' dinheiro limpo',
+	['Notification'] 		= 'Lavando Dinheiro',
+	['wash'] 				= 'lavar seu dinheiro sujo',
+	['wash_money_amount']   = 'quantia para ser lavada',
+	['invalid_amount']      = 'quantia Inválida',
+}
