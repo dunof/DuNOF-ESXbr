dd9b71e8b26cac19b74ad979fdc953c1ba281607
dunof <36583033+dunof@users.noreply.github.com>
2018-03-09 15:52:45 -0300

    es_extend
    paycheckinterval and inventoryitemdelay

--- resources/[essential]/es_extended/config.lua
+++ resources/[essential]/es_extended/config.lua
@@ -3,10 +3,10 @@ Config.MaxPlayers               = 32
 Config.Accounts                 = { 'bank', 'black_money' }
 Config.AccountLabels            = { bank = 'Banco', black_money = 'Dinheiro Sujo' } -- French
 -- Config.AccountLabels            = { bank = 'Bank', black_money = 'Dirty Money' } -- English
-Config.PaycheckInterval         = 7 * 60000
+Config.PaycheckInterval         = 30 * 60000
 Config.ShowDotAbovePlayer       = false
 Config.DisableWantedLevel       = true
-Config.RemoveInventoryItemDelay = 5 * 60000
+Config.RemoveInventoryItemDelay = 2 * 60000
 Config.EnableWeaponPickup       = true
 Config.Locale                   = 'br'
 Config.EnableDebug              = false
