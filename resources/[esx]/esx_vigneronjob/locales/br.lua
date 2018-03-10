82f43685efb250cc9b2f83d8c21db3ef4a2c32a0
dunof <36583033+dunof@users.noreply.github.com>
2018-03-10 01:55:10 -0300

    esx_vigneronjob

---
+++ resources/[esx]/esx_vigneronjob/locales/br.lua
@@ -0,0 +1,47 @@
+-----------------------------------------
+-- Created and modify by L'ile Légale RP
+-- SenSi and Kaminosekai
+-----------------------------------------
+Locales['br'] = {
+	-- General
+	['cloakroom'] = 'Vestiário',
+	['vine_clothes_civil'] = 'sua Roupa',
+	['vine_clothes_vine'] = 'roupa de Vinicultor',
+	['veh_menu'] = 'garagem',
+	['not_enough_place'] = 'você não tem espaço suficiente!',
+	['sale_in_prog'] = 'venda em andamento...',
+	['van'] = 'caminhão de trabalho',
+	['open_menu'] = ' ',
+	['spawn_veh'] = 'pressione ~INPUT_CONTEXT~ para pegar o veículo',
+	['amount_invalid'] = 'quantidade inválida',
+	['press_to_open'] = 'pressione ~INPUT_CONTEXT~ para acessar o menu',
+	['billing'] = 'faturamento',
+	['invoice_amount'] = 'valor da fatura',
+	['no_players_near'] = 'sem jogadores nas proximidades',
+	['store_veh'] = 'pressione ~INPUT_CONTEXT~ para guardar o veículo',
+	['comp_earned'] = 'sua sociedade ganhou ~g~$',
+	['deposit_stock'] = 'depositar no Estoque',
+	['take_stock'] = 'pegar do Estoque',
+	['boss_actions'] = 'ações de Chefe',
+	['quantity'] = 'quantidade',
+	['quantity_invalid'] = 'quantidade Inválida',
+	['inventory'] = 'inventário',
+	['have_withdrawn'] = 'você retirou x',
+	['added'] = 'você adicionou x',
+	
+	-- Depend to the company
+	['vigneron_client'] = 'cliente Vinícula',
+	['transforming_in_progress'] = 'processamento da uva em andamento ... ',
+	['raisin_taken'] = 'você está colhendo uvas',
+	['press_traitement'] = 'pressione ~INPUT_CONTEXT~ para processar a uva',
+	['press_collect'] = 'pressione ~INPUT_CONTEXT~ para coletar a uva',
+	['press_sell'] = 'pressione ~INPUT_CONTEXT~ para vender seus produtos',
+	['no_jus_sale'] = 'você não tem mais sucos de uva',
+	['no_vin_sale'] = 'você não tem mais vinhos',
+	['not_enough_raisin'] = 'você não tem uva suficiente',
+	['grand_cru'] = '~g~Impressionante, uma garrafa vintage!~w~',
+	['no_product_sale'] = 'você não tem mais produtos',
+	['press_traitement_jus'] = 'pressione ~INPUT_CONTEXT~ para processar suas uvas',
+	['used_jus'] = 'você bebeu um suco de uva',
+	['used_grand_cru'] = 'você bebeu uma garrafa de vinho vintage',
+}
