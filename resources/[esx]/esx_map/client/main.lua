c1bce39be8b3ad527bd38c73b36b832ce4eedaca
Flávio J. Félix <36583033+dunof@users.noreply.github.com>
2018-03-13 02:00:13 -0300

    esx_map

---
+++ resources/[esx]/esx_map/client/main.lua
@@ -0,0 +1,19 @@
+Citizen.CreateThread(function()
+	
+
+	for i=1, #Config.Map, 1 do
+		
+		local blip = AddBlipForCoord(Config.Map[i].x, Config.Map[i].y, Config.Map[i].z)
+		SetBlipSprite (blip, Config.Map[i].id)
+		SetBlipScale  (blip, 1.2)
+		SetBlipDisplay(blip, 4)
+		SetBlipColour (blip, Config.Map[i].color)
+		SetBlipAsShortRange(blip, true)
+
+		BeginTextCommandSetBlipName("STRING")
+		AddTextComponentString(Config.Map[i].name)
+		EndTextCommandSetBlipName(blip)
+	end
+
+
+end)
