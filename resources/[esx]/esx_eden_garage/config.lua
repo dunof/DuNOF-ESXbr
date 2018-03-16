b4ec8a5524a719ddbdda2c66a1fc409cf3373ee8
dunof <36583033+dunof@users.noreply.github.com>
2018-03-15 21:04:20 -0300

    some fixes

--- resources/[esx]/esx_eden_garage/config.lua
+++ resources/[esx]/esx_eden_garage/config.lua
@@ -5,7 +5,7 @@ Config = {
 		Sprite	= 50,
 		Scale	= 0.6,
 		Color	= 39 
-	},
+	}
 }
 
 Config.Garages = {
@@ -26,11 +26,10 @@ Config.Garages = {
 			Pos = {x=215.124, y=-791.377, z=29.646},
 			Color = {r=255,g=0,b=0},
 			Size  = {x = 3.0, y = 3.0, z = 1.0},
-			Marker = 1,
-			
-		},
+			Marker = 1	
+		}
 	},
-	Garage_Pone = {
+	Garage_PPopular = {
 		Name	= "Estacionamento Público",	
 		Pos 	= {x=312.08581542968, y=-1080.509765625, z=29.402994155884},
 		Size  	= {x = 1.5, y = 1.5, z = 1.0},
@@ -47,11 +46,10 @@ Config.Garages = {
 			Pos = {x=306.62, y=-1080.96, z=28.186},
 			Color = {r=255,g=0,b=0},
 			Size  = {x = 2.0, y = 2.0, z = 1.0},
-			Marker = 1,
-			
-		},
+			Marker = 1
+		}
 	},
-	Garage_P2 = {
+	Garage_RichardMajestic = {
 		Name	= "Garagem",	
 		Pos 	= {x=-881.35, y=-352.84, z=34.33},
 		Size  	= {x = 1.5, y = 1.5, z = 1.0},
@@ -68,30 +66,28 @@ Config.Garages = {
 			Pos = {x=-889.03, y=-336.85, z=33.326},
 			Color = {r=255,g=0,b=0},
 			Size  = {x = 2.0, y = 2.0, z = 1.0},
-			Marker = 1,
-			
-		},
+			Marker = 1
+		}
 	},
 	Garage_DelPerroHeights = {
 		Name	= "Garagem",	
-		Pos 	= {x=-1453.31, y=-507.57, z=31.16},
+		Pos 	= {x=-1453.31, y=-507.57, z=30.376},
 		Size  	= {x = 1.5, y = 1.5, z = 1.0},
 		Color 	= {r = 204, g = 204, b = 0},
 		Marker 	= 1,
 		SpawnPoint = {
-			Pos = {x=-1451.72, y=-507.52, z=31.16}, 
+			Pos = {x=-1451.72, y=-507.52, z=30.376}, 
 			Color = {r=0,g=255,b=0},
 			Size  = {x = 1.5, y = 1.5, z = 1.0},
 			Heading=35.36,
 			Marker = 1		
 		},
 		DeletePoint = {
-			Pos = {x=-1454.41, y=-507.85, z=31.16},
+			Pos = {x=-1454.41, y=-507.85, z=30.376},
 			Color = {r=255,g=0,b=0},
 			Size  = {x = 2.0, y = 2.0, z = 1.0},
-			Marker = 1,
-			
-		},
+			Marker = 1
+		}
 	},
 	Garage_vehshop = {
 		Name	= "Estacionamento Público",	
@@ -110,8 +106,7 @@ Config.Garages = {
 			Pos = {x=-56.35, y=-1116.85, z=25.226},
 			Color = {r=255,g=0,b=0},
 			Size  = {x = 2.0, y = 2.0, z = 1.0},
-			Marker = 1,
-			
-		},
-	},
+			Marker = 1
+		}
+	}
 }
